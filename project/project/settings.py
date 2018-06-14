0c4d1b623783a1e69c4789a94409632c0691fbf7
louis1022 <konuma1022@gmail.com>
2018-06-14 14:57:19 +0900

    startproject

--- project/project/settings.py
+++ project/project/settings.py
@@ -20,33 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '03=rwjiw7*8qbe9t!x6x%cl7q@y855l5o30%6hkzx*!@z%ey07'
+SECRET_KEY = ')%5r+@c%py4o#deg*@ak-^4cr94k1=drfxio9af@$mu#x4q1^p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-if DEBUG:
-    INTERNAL_IPS = ['127.0.0.1', 'localhost']
-
-    def custom_show_toolbar(request):
-        return True
-
-    DEBUG_TOOLBAR_PANELS = [
-        'debug_toolbar.panels.timer.TimerPanel',
-        'debug_toolbar.panels.request.RequestPanel',
-        'debug_toolbar.panels.sql.SQLPanel',
-        'debug_toolbar.panels.templates.TemplatesPanel',
-        'debug_toolbar.panels.cache.CachePanel',
-        'debug_toolbar.panels.logging.LoggingPanel',
-    ]
-
-    DEBUG_TOOLBAR_CONFIG = {
-        'INTERCEPT_REDIRECTS': False,
-        'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
-        'HIDE_DJANGO_SQL': False,
-        'TAG': 'div',
-        'ENABLE_STACKTRACES': True,
-    }
 
 ALLOWED_HOSTS = []
 
@@ -60,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',  # 追加部分
-    'manager',
 ]
 
 MIDDLEWARE = [
@@ -72,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',  # 追加
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -99,23 +73,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql', # postgreSQLの場合：'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'django_db',
-#         'USER': 'django_user',
-#         'PASSWORD': 'password',
-#         'HOST': '',
-#         'PORT': '',
-#     }
-# }
-'''SQLite環境での実行時'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -154,10 +118,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Static file settings
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
