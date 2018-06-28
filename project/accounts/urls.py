3d034bcbbba0dccc7d9a9678e72caed942453e6f
louis1022 <konuma1022@gmail.com>
2018-06-28 13:32:20 +0900

    auth機能の追加

---
+++ project/accounts/urls.py
@@ -0,0 +1,22 @@
+import django.contrib.auth.views
+from django.urls import path,include
+from . import views
+
+
+app_name='accounts'
+
+urlpatterns=[
+    path('top/',views.top_page, name="top"),
+    path('login/', # ログイン
+         django.contrib.auth.views.login,
+         {
+             'template_name': 'user_auth/login.html',
+         },
+         name='login'),
+    path('logout/', # ログアウト
+         django.contrib.auth.views.logout,
+         {
+             'template_name': 'user_auth/logout.html',
+         },
+         name='logout'),
+]
